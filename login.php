575b5da0ee3dd56c17747402e81557b7abc796a5
talel <talelmejri8@gmail.com>
2022-07-29 18:41:09 +0100

    form validation

--- login.php
+++ login.php
@@ -1,5 +1,36 @@
 <?php 
   include "connect_db.php";
+  $errors=[];
+  if(isset($_POST['submit'])){
+    extract($_POST);
+    if(empty($email) && empty($password)){
+        $errors[0]="all field is empty";
+        goto show_form;
+    }
+    if(empty($email)){
+        $errors['email']="filed email is empty";
+        goto show_form;
+    }
+    if(empty($password)){
+        $errors['password']="filed password is empty";
+        goto show_form;
+    }
+    if(filter_var($email,FILTER_VALIDATE_EMAIL)==false){
+        $errors['email']="invalid email";
+        goto show_form;
+    }
+    if(strlen($password)<6){
+        $errors['password']="password must be at least 6 characters";
+        goto show_form;
+    }
+    if(empty($errors)){
+        $query=$pdo->prepare("SELECT * FROM users where email=:email");
+        $query->execute(['email'=>$email]);
+        $users=$query->fetch();
+        var_dump($users);
+    }
+  }
+  show_form:
   $template="login";
   $page_titel="login page";
   include "./layout.phtml";

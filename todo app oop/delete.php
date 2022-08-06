5d918ee76afbbe93c55e83400d9e1bafb9138e72
talel <talelmejri8@gmail.com>
2022-08-07 00:27:53 +0100

    fix

--- todo app oop/delete.php
+++ todo app oop/delete.php
@@ -1,6 +1,5 @@
 <?php 
     include './classes/todo.php';
-
     $todo=new todo;
     $todo->delete($_GET['id']);
     header("location:index.php?type=success&msg=delete todo succefuly");
--- todo app oop/detail.php
+++ todo app oop/detail.php
@@ -1,10 +1,5 @@
 <?php
 
-  if(! array_key_exists('id',$_GET) or !ctype_digit($_GET['id'])){
-        header("location:index.php");
-        exit();
-    }
-
     include './classes/Todo.php';
     $todo=new todo();
     $todo=$todo->get($_GET['id']);

0fdc9c6ef6708d11357fc44653b53b311ddce710
talel <talelmejri8@gmail.com>
2022-08-06 22:42:26 +0100

    create data base

---
+++ todo app oop/classes/database.php
@@ -0,0 +1,29 @@
+<?php 
+    
+     class DataBase extends PDO
+     {
+        const DB_HOST="localhost";
+        const DB_NAME="todos";
+        const DB_USER="root";
+        const DB_PASSWORD="";
+
+      public function __construct()
+         {
+            $dsn='mysql:host='.self::DB_HOST.';dbname='.self::DB_NAME;
+        try{
+            //new PDO($dsn,self::DB_USER,self::DB_PASSWORD);
+                  //ou bien
+            parent::__construct($dsn,self::DB_USER,self::DB_PASSWORD);
+            $this->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+            $this->query('SET NAMES UTF8');
+        }catch(Exception $e){
+            echo 'connect failed :'.$e->getMessage();
+        }
+      }
+
+     }
+
+
+
+?>
---
+++ todo app oop/index.php
@@ -0,0 +1,4 @@
+<?php 
+    include './classes/database.php';
+    new DataBase();
+?>
